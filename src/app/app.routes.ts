8ed7fd976a27d082c0e79418136e8b103311938f
mithilesh <mithilesht@cloudthat.com>
2024-03-23 17:35:44 +0530

    successfully created a reactive form example in angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: RegistrationFormComponent }
+];
---
+++ src/app/registration-form/registration-form.component.ts
@@ -0,0 +1,73 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration-form',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+
+  ],
+  templateUrl: './registration-form.component.html',
+  styleUrl: './registration-form.component.css'
+})
+export class RegistrationFormComponent implements OnInit {
+
+  isSubmitted: boolean = false;
+  formNotValid: boolean = false;
+
+  firstNameError: string = "First Name is required"
+  lastNameError: string = "Last Name is required"
+  emailError: string = "Email is required"
+  contactError: string = "Contact is required"
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  registerForm: FormGroup = new FormGroup({
+    firstname: new FormControl(''),
+    lastname: new FormControl(''),
+    email: new FormControl(''),
+    contact: new FormControl(''),
+  });
+
+  // registerForm = this.fb.group({
+  //   firstname: ['', Validators.required],
+  //   lastname: ['', Validators.required],
+  //   email: ['', Validators.required, Validators.email],
+  //   contact: ['', Validators.required]
+  // })
+
+  ngOnInit(): void {
+
+    this.registerForm = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      contact: ['', Validators.required]
+    })
+
+  }
+
+  onSubmit() {
+
+    console.log(this.registerForm.get('firstname')?.invalid)
+    console.log(
+      this.registerForm.value,
+      this.registerForm.invalid
+    )
+    // if (this.registerForm.get('firstname')?.invalid) {
+    //   this.formNotValid = true
+    // }
+
+    if (this.registerForm.invalid) {
+      this.formNotValid = true
+    }
+
+
+  }
+
+}
